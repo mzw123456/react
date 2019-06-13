93c2345b361fee314c07559db40a1b87dd2353dc
mengziwie <2585556469@qq.com>
2019-06-13 11:36:49 +0800

    在render中直接事件直接改变this指向

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from  'react'
 class App extends Component {
   constructor (props) {
     super(props)
     console.log(1,this)
-    this.fn=this.fn.bind(this)
   }
   fn () {
-    console.log(2,this) 
+    console.log(2,this)
   }
   render () {
     return (
       <div>
-        <button onClick={this.fn}>我是一个按钮</button>
+        <button onClick={this.fn.bind(this)}>我是一个按钮</button>
       </div>
     )
   }
